6fe78159cd7c6bbb23f7adbc311b8c511d81b4f6
D. Nikolov <d.nikolov7654@gmail.com>
2024-12-12 10:06:58 +0200

    some task 10 features

--- 10/main.c
+++ 10/main.c
@@ -10,6 +10,68 @@
 #include <fcntl.h>
 
 #include <err.h>
+#include <stdbool.h>
+
+#include "queue.c"
+
+struct coords {
+        int x, y;
+};
+
+struct coords coords_new(int x, int y)
+{
+        return (struct coords){
+                .x = x,
+                .y = y,
+        };
+}
+
+bool coords_valid(const struct coords c, size_t width, size_t height)
+{
+        return c.x > 0 && c.x < width && c.y > 0 && c.y < height;
+}
+
+bool coords_eq(const struct coords left, const struct coords right)
+{
+        return left.x == right.x && left.y == right.y;
+}
+
+struct coords coords_added(const struct coords a, const struct coords b)
+{
+        return coords_new(a.x + b.x, a.y + b.y);
+}
+
+/* a += b */
+void coords_add(struct coords *a, const struct coords b)
+{
+        a->x += b.x;
+        a->y += b.y;
+}
+
+char coords_idx(const char **input, const struct coords c)
+{
+        return input[c.y][c.x];
+}
+
+struct trail {
+        struct coords start;
+        struct coords current;
+};
+
+struct trail *trail_alloc(struct coords start, struct coords current)
+{
+        struct trail *result = (struct trail *)malloc(sizeof(struct trail));
+        result->start = start;
+        result->current = current;
+        return result;
+}
+
+bool trail_eq(void *left, void *right)
+{
+        struct trail *l = left;
+        struct trail *r = right;
+        return coords_eq(l->start, r->start) && coords_eq(l->current, r->current);
+}
 
 char **get_input(const char *filename)
 {
@@ -17,18 +79,18 @@ char **get_input(const char *filename)
         size_t result_len = 0, line_len;
         char *line_start, *line_end, *file_raw;
         char **result;
-        struct stat stat;
+        struct stat file_stat;
 
         if ((fd = open(filename, O_RDONLY)) < 0) {
                 return NULL;
         }
 
-        if (fstat(fd, &stat) < 0) {
+        if (fstat(fd, &file_stat) < 0) {
                 close(fd);
                 return NULL;
         }
 
-        file_raw = (char *)mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        file_raw = (char *)mmap(0, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
         close(fd);
         if (file_raw == (void *)-1) {
                 return NULL;
@@ -41,7 +103,7 @@ char **get_input(const char *filename)
         }
 
         line_len = line_end - file_raw;
-        result_len = stat.st_size / line_len;
+        result_len = file_stat.st_size / line_len;
 
         /* an extra NULL pointer is allocated to the end */
         result = calloc(result_len + 1, sizeof(char *));
@@ -51,40 +113,82 @@ char **get_input(const char *filename)
 
         for (int i = 0; line_end != NULL; ++i) {
                 result[i] = strndup(line_start, line_len + 1);
-                result[i][line_len + (i > 0)] = '\0';
-                line_start = line_end;
-                line_end = strchr(line_end + 1, '\n');
+                result[i][line_len] = '\0';
+                /* jump over the current newline */
+                line_start = line_end + 1;
+                line_end = strchr(line_start, '\n');
         }
 
+        munmap(file_raw, file_stat.st_size);
         return result;
 }
 
-typedef struct dll_node {
-        void *value;
-        struct dll_node *next, *prev;
-} DllNode; /* doubly linked list*/
-
-DllNode *dll_node_init(void *value)
+void get_input_dimensions(const char **input, unsigned *width, unsigned *height)
 {
-        DllNode *res = malloc(sizeof(DllNode));
-        if (NULL == res)
-                return NULL;
-        res->value = value;
-        res->prev = NULL;
-        res->next = NULL;
-        return res;
+        *width = strlen(input[0]);
+        *height = 0;
+        for (const char **line = input; *line != NULL; line += 1)
+                *height += 1;
 }
 
-typedef struct {
-        DllNode *head, *tail;
-} Queue;
+int get_trail_scores_sum(const char **input)
+{
+        unsigned width, height;
+        struct queue bfs_queue = queue_init();
+
+        get_input_dimensions(input, &width, &height);
+        for (int y = 0; y < height; ++y) {
+                for (int x = 0; x < width; ++x) {
+                        if (input[y][x] == '0') {
+                                struct coords origin = coords_new(x, y);
+                                queue_push(&bfs_queue, trail_alloc(origin, origin));
+                        }
+                }
+        }
+
+        struct coords directions[4] = {
+                coords_new(-1, 0),
+                coords_new(1, 0),
+                coords_new(0, -1),
+                coords_new(0, 1),
+        };
+
+        struct dll traversed_trails = dll_new();
+        struct trail *iter = queue_pop(&bfs_queue);
+
+        unsigned int trails_count = 0;
+        for (; iter != NULL; iter = queue_pop(&bfs_queue)) {
+                if (coords_idx(input, iter->current) == '9') {
+                        if (!dll_contains(&traversed_trails, iter, trail_eq)) {
+                                trails_count += 1;
+                                dll_push_back(&traversed_trails, iter);
+                        }
+                        continue;
+                }
+                for (int i = 0; i < 4; ++i) {
+                        struct coords current = coords_added(iter->current, directions[i]);
+                        if (!coords_valid(current, width, height) &&
+                            coords_idx(input, current) == coords_idx(input, iter->current) + 1)
+                                continue;
+                        queue_push(&bfs_queue, trail_alloc(iter->start, current));
+                }
+                free(iter);
+        }
+
+        /* free the traversed_trails DLL */
+        return trails_count;
+}
 
 int main()
 {
         printf("hello\n");
         char **input = get_input("input.txt");
+        for (char **line = input; *line; line++) {
+                printf("%s\n", *line);
+        }
         if (NULL == input) {
                 err(1, "could not get input");
         }
+        printf("%d\n", get_trail_scores_sum((const char **)input));
         return 0;
 }
